87293e18abd20cb52e30cf025f595641fc9cbc4a
sabarics <sabarics10@gmail.com>
2020-04-23 18:24:56 +0530

    initial commit, Cloned from bitbucket repo

---
+++ ZuperForms/ZuperFormsSdk/ZuperFormsSdk/ZuperFormsSdk.h
@@ -0,0 +1,19 @@
+//
+//  ZuperFormsSdk.h
+//  ZuperFormsSdk
+//
+//  Created by Zuper on 18/04/20.
+//  Copyright © 2020 Zuper. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZuperFormsSdk.
+FOUNDATION_EXPORT double ZuperFormsSdkVersionNumber;
+
+//! Project version string for ZuperFormsSdk.
+FOUNDATION_EXPORT const unsigned char ZuperFormsSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZuperFormsSdk/PublicHeader.h>
+
+
